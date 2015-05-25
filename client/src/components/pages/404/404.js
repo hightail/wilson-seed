a986cc24263ec1ac655a6e0d641031211b408f88
hunternovak <hunternovak@gmail.com>
2015-05-25 13:32:59 -0700

    Initial Seed Project Commit.

---
+++ client/src/components/pages/404/404.js
@@ -0,0 +1,21 @@
+/**
+ * 404 Component
+ *
+ * @class 404Component
+ * @module Hightail
+ * @submodule Hightail.Components
+ *
+ * @example
+ *    <ht-404></ht-404>
+ *
+ * @author justin.fiedler
+ * @since 0.0.1
+ *
+ * @copyright (c) 2013 Hightail Inc. All Rights Reserved
+ */
+'use strict';
+
+angular.wilson.component('404', {
+  page: true,
+  controller: $.noop
+});
---
+++ client/src/components/pages/home/home.js
@@ -0,0 +1,33 @@
+/**
+ * home Component
+ *
+ * @class HomeComponent
+ *
+ * @example
+ *    <ht-home></ht-home>
+ *
+ * @author hunter.novak
+ * @since 0.0.1
+ *
+ */
+'use strict';
+
+angular.wilson.component('home', {
+  page: true,
+  controller: ['$scope',
+    function($scope) {
+      var controller = this;
+
+      //  controller.setState({
+      //    initial: '',
+      //    events: [
+      //      { name: '',  from: '',  to: '' }
+      //    ],
+      //    timeouts: [],
+      //    callbacks: {}
+      //  });
+
+    }
+  ]
+  
+});
