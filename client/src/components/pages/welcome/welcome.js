73e8b92a14997a489c9f893669e6b5f409affb28
Hunter Novak <hunternovak@gmail.com>
2015-05-26 17:50:07 -0700

    Latest adjustments. Added welcome page.

---
+++ client/src/components/pages/welcome/welcome.js
@@ -0,0 +1,36 @@
+/**
+ * welcome Component
+ *
+ * @class WelcomeComponent
+ * @module Hightail
+ * @submodule Hightail.Components
+ *
+ * @example
+ *    <ht-welcome></ht-welcome>
+ *
+ * @author hunter.novak
+ * @since 0.0.1
+ *
+ * @copyright (c) 2015 Hightail Inc. All Rights Reserved
+ */
+'use strict';
+
+angular.wilson.component('welcome', {
+  page: true,
+  controller: ['$scope',
+    function($scope) {
+      var controller = this;
+
+      //  controller.setState({
+      //    initial: '',
+      //    events: [
+      //      { name: '',  from: '',  to: '' }
+      //    ],
+      //    timeouts: [],
+      //    callbacks: {}
+      //  });
+
+    }
+  ]
+  
+});
